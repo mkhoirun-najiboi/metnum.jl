524fc6a73b795ff23f4d1a2b6a61ec0f64a9f180
MOHAMAD KHOIRUN NAJIB <najib_mathipb@apps.ipb.ac.id>
2021-12-20 06:13:42 +0700

    update

--- src/metnum.jl
+++ src/metnum.jl
@@ -67,21 +67,4 @@ include("newtonRaphson.jl")
 include("panas.jl")
 include("reglin.jl")
 
-
-function mulaipraktikum()
-    using Dates
-    repo_directory = joinpath(@__DIR__,".julia","packages","metnum")
-    folders = readdir(repo_directory)
-    unix_time = Array{Any}(undef,length(folders))
-    k=1
-    for folder in folders
-        date_time = unix2datetime(mtime(joinpath(repo_directory, folder)))
-        unix_time[k] = Int(floor(datetime2unix(date_time)))
-        k+=1
-    end
-    path = joinpath(repo_directory, folders[argmax(unix_time)],"notebookpraktikum")
-    IJulia.notebook(;dir=path)
-end
-
-
 end
