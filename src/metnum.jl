b2db642e3b5532e13440d924bd444baee1807856
MOHAMAD KHOIRUN NAJIB <najib_mathipb@apps.ipb.ac.id>
2021-12-20 06:06:07 +0700

    add mulaipraktikum

--- src/metnum.jl
+++ src/metnum.jl
@@ -18,6 +18,8 @@ module metnum
 
 using LinearAlgebra
 using Statistics
+using Dates
+using IJulia
 
 export regpoly, regpower, regulaFalsi, rekons, rekursif, richardson, rkf45
 export romberg, rungekutta, rungekuttasistem, secant
@@ -64,6 +66,22 @@ include("newton.jl")
 include("newtonRaphson.jl")
 include("panas.jl")
 include("reglin.jl")
+
+
+function mulaipraktikum()
+    using Dates
+    repo_directory = joinpath(@__DIR__,".julia","packages","metnum")
+    folders = readdir(repo_directory)
+    unix_time = Array{Any}(undef,length(folders))
+    k=1
+    for folder in folders
+        date_time = unix2datetime(mtime(joinpath(repo_directory, folder)))
+        unix_time[k] = Int(floor(datetime2unix(date_time)))
+        k+=1
+    end
+    path = joinpath(repo_directory, folders[argmax(unix_time)],"notebookpraktikum")
+    IJulia.notebook(;dir=path)
+end
 
 
 end
