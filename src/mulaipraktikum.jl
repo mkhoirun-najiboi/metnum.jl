317c9cc8783de4179f40ce19541f5d8549591e12
MOHAMAD KHOIRUN NAJIB <najib_mathipb@apps.ipb.ac.id>
2021-12-20 06:26:08 +0700

    fix bug

--- src/mulaipraktikum.jl
+++ src/mulaipraktikum.jl
@@ -1,13 +1,5 @@
 function mulaipraktikum()
-    repo_directory = joinpath(@__DIR__,".julia","packages","metnum")
-    folders = readdir(repo_directory)
-    unix_time = Array{Any}(undef,length(folders))
-    k=1
-    for folder in folders
-        date_time = unix2datetime(mtime(joinpath(repo_directory, folder)))
-        unix_time[k] = Int(floor(datetime2unix(date_time)))
-        k+=1
-    end
-    path = joinpath(repo_directory, folders[argmax(unix_time)],"notebookpraktikum")
+    repo_directory = joinpath(@__DIR__,"..")
+    path = joinpath(repo_directory, "notebookpraktikum")
     IJulia.notebook(;dir=path)
 end
